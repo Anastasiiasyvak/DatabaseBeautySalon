c65d4aac16383d1c881d3957db9dad646cc35650
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-12 23:12:43 +0300

    Update procedures.sql

--- pa4/procedures.sql
+++ pa4/procedures.sql
@@ -4,20 +4,21 @@ CREATE PROCEDURE employee_details_by_id(IN emp_id INT)
 BEGIN
     SELECT name, phone, email, position, salary FROM employee WHERE id = emp_id;
 END $$
+$$
 DELIMITER ;
 
 -- Procedure to obtain total product quantity and average product cost
 DELIMITER $$
 CREATE PROCEDURE total_quantity_and_avg_cost(OUT total_quantity INT, OUT avg_cost DOUBLE)
 BEGIN
-    SELECT SUM(quantity) INTO total_quantity FROM product;
-    SELECT AVG(cost) INTO avg_cost FROM product;
+    SELECT SUM(quantity), AVG(cost) INTO total_quantity, avg_cost FROM product;
 END $$
+$$
 DELIMITER ;
 
 -- Procedure for changing an employee's position and calculating a new average salary
 DELIMITER $$
-CREATE PROCEDURE update_position_and_avg_salary(INOUT avg_salary DOUBLE, IN emp_id INT, IN new_position VARCHAR(255))
+CREATE PROCEDURE update_position_and_avg_salary(OUT new_avg_salary DOUBLE, IN emp_id INT, IN new_position VARCHAR(255))
 BEGIN
     DECLARE old_salary DOUBLE;
 
@@ -27,12 +28,15 @@ BEGIN
     UPDATE employee SET position = new_position WHERE id = emp_id;
 
     IF new_position = 'Administrator' THEN
-        SET avg_salary = (SELECT AVG(salary) FROM employee); 
+        SELECT AVG(salary) INTO new_avg_salary FROM employee; 
+    ELSE
+        SET new_avg_salary = (SELECT AVG(salary) FROM employee);
     END IF;
 
     COMMIT;
 
     SELECT name, position, salary FROM employee WHERE id = emp_id;
 END $$
+$$
 DELIMITER ;
 
