a3782bb01a288e480661d6403a14141d278997bd
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-12 23:16:40 +0300

    Update procedures.sql

--- pa4/procedures.sql
+++ pa4/procedures.sql
@@ -1,24 +1,22 @@
 -- Procedure for retrieving employee details by employee ID
 DELIMITER $$
-CREATE PROCEDURE employee_details_by_id(IN emp_id INT)
+CREATE PROCEDURE employee_details_by_id_sproc(IN emp_id INT)
 BEGIN
     SELECT name, phone, email, position, salary FROM employee WHERE id = emp_id;
 END $$
-$$
 DELIMITER ;
 
 -- Procedure to obtain total product quantity and average product cost
 DELIMITER $$
-CREATE PROCEDURE total_quantity_and_avg_cost(OUT total_quantity INT, OUT avg_cost DOUBLE)
+CREATE PROCEDURE total_quantity_and_avg_cost_sproc(OUT total_quantity INT, OUT avg_cost DOUBLE)
 BEGIN
     SELECT SUM(quantity), AVG(cost) INTO total_quantity, avg_cost FROM product;
 END $$
-$$
 DELIMITER ;
 
 -- Procedure for changing an employee's position and calculating a new average salary
 DELIMITER $$
-CREATE PROCEDURE update_position_and_avg_salary(OUT new_avg_salary DOUBLE, IN emp_id INT, IN new_position VARCHAR(255))
+CREATE PROCEDURE update_position_and_avg_salary_sproc(OUT new_avg_salary DOUBLE, IN emp_id INT, IN new_position VARCHAR(255))
 BEGIN
     DECLARE old_salary DOUBLE;
 
@@ -37,6 +35,5 @@ BEGIN
 
     SELECT name, position, salary FROM employee WHERE id = emp_id;
 END $$
-$$
 DELIMITER ;
 
